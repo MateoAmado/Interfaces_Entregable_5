b28e71bd34ab60f06b6d9cb38a2c25d13a884cbf
Gonzalo <gonzalohellers@gmail.com>
2024-07-04 14:50:22 -0300

    Login mejorado

--- sesionHelper.js
+++ sesionHelper.js
@@ -9,10 +9,24 @@
         }
         return false;
     }
+    const conditionalLink = document.getElementById('conditionalLink');
+    const conditionalLink2 = document.getElementById('conditionalLinklogout');
 
     document.addEventListener('DOMContentLoaded', function () {
-        const conditionalLink = document.getElementById('conditionalLink');
         if (comprobarSesion()) {
-            conditionalLink.innerHTML = '<a href="agregarNoticia.html"><i class="fa-solid fa-plus"></i>Agregar Noticia</a>';
+            conditionalLink.innerHTML = '<i class="fa-solid fa-plus"></i>Agregar Noticia';
+            conditionalLink.href="agregar.html"
+            conditionalLink2.addEventListener("click",()=>{
+                desloguear(false);
+            })
+            conditionalLink2.href="index.html"
+            conditionalLink2.innerHTML='<i class="fa-solid fa-right-to-bracket"></i>Desloguear'
         }
     });
+    function desloguear(){
+        localStorage.setItem("sesion", false);
+        conditionalLink.innerHTML='<i class="fa-solid fa-plus"></i>Forestaton'
+        conditionalLink2.href='index.html'
+        conditionalLink2.href="logIn.html"
+        conditionalLink2.innerHTML='<i class="fa-solid fa-right-to-bracket"></i>Iniciar sesión'
+    }
