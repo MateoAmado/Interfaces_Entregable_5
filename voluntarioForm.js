75a5864236e287bba3d29dff3824d0be899d1daf
Carolina Larralde <carogise9@gmail.com>
2024-07-10 15:49:14 -0300

    Control de inputs en Voluntariado. Pagina de voluntariado completada :)

--- voluntarioForm.js
+++ voluntarioForm.js
@@ -7,38 +7,73 @@ const btnBackUno = document.getElementById("back1");
 const btnBackDos = document.getElementById("back2");
 const btnEnvio = document.getElementById("botonEnvio");
 const barraProgreso = document.getElementById("progreso");
-const introduccionformParrafo = document.querySelector('.introduccionform p');
+let inputEmail = document.getElementById("emailInput");
+let inputNombre = document.getElementById("nameInput");
+let inputApellido = document.getElementById("surnameInput");
+const introduccionformParrafo = document.querySelector(".introduccionform p");
 
 //Primer Paso
-btnNextUno.addEventListener("click", ()=>{
-    formUno.style.left ="-800px";
-    formDos.style.left ="40px";
+btnNextUno.addEventListener("click", () => {
+  if (inputEmail.value.trim() === "") {
+    inputEmail.classList.add("error-input");
+    introduccionformParrafo.textContent =
+      "En orden de poder enviar el formulario, es necesario que se completen todos los campos";
+  } else {
+    formUno.style.left = "-800px";
+    formDos.style.left = "40px";
     barraProgreso.style.width = "240px";
+    introduccionformParrafo.textContent = "";
+  }
 });
-btnBackUno.addEventListener("click", ()=>{
-    formUno.style.left ="40px";
-    formDos.style.left ="800px";
-    barraProgreso.style.width = "120px";
+btnBackUno.addEventListener("click", () => {
+  formUno.style.left = "40px";
+  formDos.style.left = "800px";
+  barraProgreso.style.width = "120px";
 });
 
 //Segundo Paso
-btnNextDos.addEventListener("click", ()=>{
-    formDos.style.left ="-800px";
-    formTres.style.left ="40px";
+btnNextDos.addEventListener("click", () => {
+  if (inputNombre.value.trim() === "") {
+    inputNombre.classList.add("error-input");
+    introduccionformParrafo.textContent =
+      "En orden de poder enviar el formulario, es necesario que se completen todos los campos";
+  } else if (inputApellido.value.trim() === "") {
+    inputApellido.classList.add("error-input");
+    introduccionformParrafo.textContent =
+      "En orden de poder enviar el formulario, es necesario que se completen todos los campos";
+  } else {
+    formDos.style.left = "-800px";
+    formTres.style.left = "40px";
     barraProgreso.style.width = "360px";
+    introduccionformParrafo.textContent = "";
+  }
 });
-btnBackDos.addEventListener("click", ()=>{
-    formDos.style.left ="40px";
-    formTres.style.left ="800px";
-    barraProgreso.style.width = "240px";
+btnBackDos.addEventListener("click", () => {
+  formDos.style.left = "40px";
+  formTres.style.left = "800px";
+  barraProgreso.style.width = "240px";
 });
 
 //Enviar Formulario
 btnEnvio.addEventListener("click", (event) => {
-    event.preventDefault();
-    introduccionformParrafo.textContent = "Le agradecemos por su colaboración, en los siguientes días un miembro del equipo se contactará con usted";
+  event.preventDefault();
+  let opciones = document.getElementsByName("colaboracion");
+  let opcionSeleccionada = false;
+  for (let a = 0; a < opciones.length; a++) {
+    if (opciones[a].checked) {
+      opcionSeleccionada = true;
+      break;
+    }
+  }
+  if (!opcionSeleccionada) {
+    introduccionformParrafo.textContent =
+      "En orden de poder enviar el formulario, es necesario la elección de una opción";
+  } else {
+    introduccionformParrafo.textContent =
+      "Le agradecemos por su colaboración, en los siguientes días un miembro del equipo se contactará con usted";
     formUno.style.left = "40px";
     formDos.style.left = "800px";
     formTres.style.left = "800px";
     barraProgreso.style.width = "120px";
+  }
 });
